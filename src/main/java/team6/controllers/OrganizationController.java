412a683e64c96de01bdc23b1c4e80e187943d05f
Dayde Arthur Reid <reiddayd@mathlab.utsc.utoronto.ca>
2018-10-18 00:11:28 -0400

    Added GetMapping to organization controller

--- src/main/java/team6/controllers/OrganizationController.java
+++ src/main/java/team6/controllers/OrganizationController.java
@@ -2,6 +2,9 @@ package team6.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import team6.models;
 
 @RestController
 public class OrganizationController {
@@ -9,6 +12,12 @@ public class OrganizationController {
     @RequestMapping("/organizations")
     public String index() {
         return "Greetings from Chedy!";
+    }
+
+    @GetMapping("/organizations/new")
+    public String organizationForm(Model model) {
+        model.addAttribute("organization", new Organization());
+        return "organizations";
     }
     
 }
