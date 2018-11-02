58df00ddab0416f35a3f79b97ebf6a550b1a16c8
Omar Chehab <omarchehab98@gmail.com>
2018-11-02 13:25:34 -0400

    Support comparing different types of numbers in EqualsComparisonOperator

--- src/main/java/team6/util/operators/comparison/EqualsComparisonOperator.java
+++ src/main/java/team6/util/operators/comparison/EqualsComparisonOperator.java
@@ -1,12 +1,22 @@
 package team6.util.operators.comparison;
 
+import java.math.BigDecimal;
+
 public class EqualsComparisonOperator implements ComparisonOperator {
     public static final String OPERATOR = "eq";
 
     public boolean compare(Object lhs, Object rhs) {
-        // TODO: Implement lenient type checking
-        // Right now, if lhs is Long and rhs is Integer and equal to same number
-        // Java .equals will return false.
+        if (lhs == null && rhs == null) {
+            return true;
+        }
+        if (lhs == null || rhs == null) {
+            return false;
+        }
+        if (lhs instanceof Number && rhs instanceof Number) {
+            BigDecimal lhsBd = new BigDecimal(((Number) lhs).doubleValue());
+            BigDecimal rhsBd = new BigDecimal(((Number) rhs).doubleValue());
+            return lhsBd.compareTo(rhsBd) == 0;
+        }
         return lhs.equals(rhs);
     }
 
