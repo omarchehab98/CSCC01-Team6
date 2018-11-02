bf7a846b8a6dc0b5c971bc25e566ccb56fbcc567
Jacob <Jacob@192.168.0.10>
2018-11-02 17:55:27 -0400

    Created ClientProfileTemplateRepository.

---
+++ src/main/java/team6/repositories/ClientProfileTemplateRepository.java
@@ -0,0 +1,9 @@
+package team6.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import team6.models.ClientProfileTemplate;
+
+public interface ClientProfileTemplateRepository extends CrudRepository<ClientProfileTemplate, Long> {
+
+}
