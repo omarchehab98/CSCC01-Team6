f6795e8f8021a8bb1adea68ed08645600bf5b1ef
Omar Chehab <omarchehab98@gmail.com>
2018-11-04 20:07:14 -0500

    Refactor TemplateController and organizations/read-single.html

--- src/main/java/team6/controllers/TemplateController.java
+++ src/main/java/team6/controllers/TemplateController.java
@@ -12,12 +12,10 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,12 +27,11 @@ import team6.models.Organization;
 import team6.models.Template;
 import team6.repositories.ClientProfileTemplateRepository;
 import team6.repositories.NARsTemplateRepository;
+import team6.repositories.OrganizationRepository;
 import team6.throwables.IllegalTemplateException;
 import team6.util.SheetAdapterWrapper;
 import team6.util.parameters.SelectParameter;
 import team6.util.parameters.WhereParameter;
-import team6.util.expressions.BooleanExpression;
-import team6.repositories.OrganizationRepository;
 
 @Controller
 public class TemplateController {
@@ -54,8 +51,8 @@ public class TemplateController {
     }
 
     @PostMapping("/templates")
-    public String uploadFile(Model model, MultipartFile file, String id, @RequestParam String templateType)
-            throws IOException, IllegalTemplateException {
+    public String createTemplateByFile(Model model, MultipartFile file, @RequestParam String organizationId,
+            @RequestParam String templateType) throws IOException, IllegalTemplateException {
         // Converting the multipart file into a filestream, to be parseable
         InputStream in = file.getInputStream();
         BufferedReader fileRead = new BufferedReader(new InputStreamReader(in));
@@ -72,7 +69,7 @@ public class TemplateController {
         templateRepository = getRepo(templateType);
 
         // get the organization for this template:
-        Long orgId = Long.parseLong(id);
+        Long orgId = Long.parseLong(organizationId);
         Optional<Organization> org = organizationRepository.findById(orgId);
         Organization organization = org.get();
 
@@ -87,44 +84,31 @@ public class TemplateController {
     }
 
     @GetMapping("/templates/NARs")
-    public String readAllNARsView(Model model,
-        @RequestParam Optional<String> select,
-        @RequestParam Optional<String> where
-    ) {
+    public String readAllNARsView(Model model, @RequestParam Optional<String> select,
+            @RequestParam Optional<String> where) {
         return templateReadList(model, select, where, new NARsTemplate(), narsTemplateRepository);
     }
 
     @GetMapping("/templates/clientProfile")
-    public String readAllClientProfileView(Model model,
-        @RequestParam Optional<String> select,
-        @RequestParam Optional<String> where
-    ) {
+    public String readAllClientProfileView(Model model, @RequestParam Optional<String> select,
+            @RequestParam Optional<String> where) {
         return templateReadList(model, select, where, new ClientProfileTemplate(), clientProfileTemplateRepository);
     }
 
-    private String templateReadList(Model model,
-        @RequestParam Optional<String> select,
-        @RequestParam Optional<String> where,
-        Template template,
-        CrudRepository repository
-    ) {
+    private String templateReadList(Model model, @RequestParam Optional<String> select,
+            @RequestParam Optional<String> where, Template template, CrudRepository repository) {
         Map<String, String> attributeToFriendlyNameMap = template.getAttributeToFriendlyNameMap();
         List<String> attributeNames = template.getAttributeNames();
         List<String> friendlyNames = template.getFriendlyNames();
         if (select.isPresent()) {
             attributeNames = SelectParameter.parse(select.get());
-            friendlyNames = attributeNames.stream()
-                .map(attributeToFriendlyNameMap::get)
-                .collect(Collectors.toList());
+            friendlyNames = attributeNames.stream().map(attributeToFriendlyNameMap::get).collect(Collectors.toList());
         }
         final Iterable<Object> allTemplates = repository.findAll();
         Iterable<Object> templates = allTemplates;
         if (where.isPresent()) {
             templates = () -> StreamSupport.stream(allTemplates.spliterator(), false)
-                    .filter(row -> WhereParameter.parse(where.get())
-                        .populateWithObject(row)
-                        .isTrue()
-                    ).iterator();
+                    .filter(row -> WhereParameter.parse(where.get()).populateWithObject(row).isTrue()).iterator();
         }
         model.addAttribute("templateName", "Needs Assessment & Referrals");
         model.addAttribute("attributeNames", attributeNames);
@@ -135,10 +119,10 @@ public class TemplateController {
 
     public CrudRepository getRepo(String templateType) {
         switch (templateType) {
-            case "clientProfile":
-                return clientProfileTemplateRepository;
-            case "NARs":
-                return narsTemplateRepository;
+        case "clientProfile":
+            return clientProfileTemplateRepository;
+        case "NARs":
+            return narsTemplateRepository;
         }
         throw new IllegalArgumentException();
     }
