c836955c4ac76a1ff3bb485174a7300baee2b486
Omar Chehab <omarchehab98@gmail.com>
2018-11-17 18:51:16 -0500

    Implement 'Add a Partner Organization' Task

--- src/main/java/team6/controllers/HomeController.java
+++ src/main/java/team6/controllers/HomeController.java
@@ -1,13 +1,20 @@
 package team6.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import team6.repositories.OrganizationRepository;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private OrganizationRepository organizationRepository;
 
     @GetMapping("/")
-    public String homeView() {
+    public String homeView(Model model) {
+        model.addAttribute("organizations", organizationRepository.findAll());
         return "index";
     }
 }
