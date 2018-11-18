8f833bbc1de56a5fa79eccd9da93c5d1d8dc9de5
Omar Chehab <omarchehab98@gmail.com>
2018-11-17 19:25:08 -0500

    Add NARs Subtask in 'Upload iCare sheet data' Task

--- src/main/java/team6/controllers/HomeController.java
+++ src/main/java/team6/controllers/HomeController.java
@@ -1,11 +1,14 @@
 package team6.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import team6.repositories.ClientProfileTemplateRepository;
+import team6.repositories.NARsTemplateRepository;
 import team6.repositories.OrganizationRepository;
 import team6.repositories.QueryRepository;
 
@@ -16,12 +19,15 @@ public class HomeController {
     @Autowired
     private ClientProfileTemplateRepository clientProfileRepository;
     @Autowired
+    private NARsTemplateRepository narsRepository;
+    @Autowired
     private QueryRepository queryRepository;
 
     @GetMapping("/")
     public String homeView(Model model) {
         model.addAttribute("organizations", organizationRepository.findAll());
         model.addAttribute("clientProfiles", clientProfileRepository.findAll());
+        model.addAttribute("nars", narsRepository.findAll());
         model.addAttribute("queries", queryRepository.findAll());
         return "index";
     }
