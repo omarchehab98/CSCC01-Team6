c41be19bc67816810c0085e91c56e2d712641ac5
Omar Chehab <omarchehab98@gmail.com>
2018-11-17 19:11:02 -0500

    Implement 'Upload iCare sheet data' Task

--- src/main/java/team6/controllers/HomeController.java
+++ src/main/java/team6/controllers/HomeController.java
@@ -5,16 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import team6.repositories.ClientProfileTemplateRepository;
 import team6.repositories.OrganizationRepository;
 
 @Controller
 public class HomeController {
     @Autowired
     private OrganizationRepository organizationRepository;
+    @Autowired
+    private ClientProfileTemplateRepository clientProfileRepository;
 
     @GetMapping("/")
     public String homeView(Model model) {
         model.addAttribute("organizations", organizationRepository.findAll());
+        model.addAttribute("clientProfiles", clientProfileRepository.findAll());
         return "index";
     }
 }
