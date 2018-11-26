3829212e096671a875df84f264591c019f09fa50
Jacob <Jacob@192.168.0.10>
2018-11-26 04:43:12 -0500

    Finished updating QueryControllerTest.

--- src/test/java/team6/controllers/QueryControllerTest.java
+++ src/test/java/team6/controllers/QueryControllerTest.java
@@ -1,6 +1,7 @@
 package team6.controllers;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -15,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import team6.models.Query;
+import team6.models.Report;
 import team6.repositories.QueryRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -61,7 +63,7 @@ public class QueryControllerTest {
 		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
 
 		ResponseEntity<String> response = 
-				restTemplate.exchange(createURL("/query/" + id),
+				restTemplate.exchange(createURL("/queries/" + id),
 				HttpMethod.GET, entity, String.class);
 
 		String expected = ViewGenerators.getReadSingleViewQueries(id);
@@ -71,7 +73,71 @@ public class QueryControllerTest {
 		queryRepository.deleteById(query.getId());
 	}
 
-	
+	/*
+	@Test
+	public void testCreate() {
+		Query query = new Query();
+		HttpEntity<Query> entity = new HttpEntity<Query>(query, headers);
+
+		ResponseEntity<String> response = 
+				restTemplate.exchange(createURL("/queries"),
+				HttpMethod.POST, entity, String.class);
+
+		String actual = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+
+		assertTrue(actual.contains("/queries"));
+	}
+	*/
+
+	public void testUpdateByIdView() {
+		Query query = new Query();
+		queryRepository.save(query);
+		String id = String.valueOf(query.getId());
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<String> response = 
+				restTemplate.exchange(createURL("/queries/" + id + 
+						"/update"), HttpMethod.GET, entity, String.class);
+
+		String expected = ViewGenerators.getUpdateViewReports(id);
+
+		assertEquals(expected, response.getBody());
+
+		queryRepository.deleteById(query.getId());
+	}
+
+	@Test
+	public void testUpdateById() throws Exception {
+		Query query = new Query();
+		queryRepository.save(query);
+		String id = String.valueOf(query.getId());
+		HttpEntity<Query> entity = new HttpEntity<Query>(query, headers);
+
+		ResponseEntity<String> response = 
+				restTemplate.exchange(createURL("/queries/" + id),
+				HttpMethod.POST, entity, String.class);
+
+		String actual = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+
+		assertTrue(actual.contains("/queries/" + id));
+		
+		queryRepository.deleteById(query.getId());
+	}
+
+	public void testDeleteById() throws Exception {
+		Query query = new Query();
+		queryRepository.save(query);
+		HttpEntity<Query> entity = new HttpEntity<Query>(query, headers);
+		String id = String.valueOf(query.getId());
+
+		ResponseEntity<String> response = 
+				restTemplate.exchange(createURL("/queries/" + id),
+				HttpMethod.DELETE, entity, String.class);
+
+		String actual = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+
+		assertFalse(actual.contains("/queries/" + id));
+	}
 
 	private String createURL(String uri) {
 		return "http://localhost:8080" + uri;
